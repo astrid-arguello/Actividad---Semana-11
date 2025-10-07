7cc352324450eed72631fd9c2bdcac940a3f7bd4
Yessica Reyes <yexierr9@gmail.com>
2025-10-07 19:37:33 -0400

    Agregar archivo Ejemplo4.py con histograma

---
+++ Ejemplo4.py
@@ -0,0 +1,17 @@
+#Histrograma
+import matplotlib.pyplot as plt
+
+
+tiempos = [10, 120, 60, 120, 20, 200, 40, 300, 90, 10, 120]
+
+
+plt.hist(tiempos, bins=8, color='purple', edgecolor='black', alpha=0.8)
+
+
+plt.title("Tiempo para llegar a la Universidad (en minutos)")
+plt.xlabel("Minutos")
+plt.ylabel("Frecuencia de estudiantes")
+
+
+plt.show()
+
